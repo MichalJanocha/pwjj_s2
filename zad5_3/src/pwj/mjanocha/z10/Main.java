e9fb2e28b13ca009744f06c6c99913949825e3ae
Michal <michal.janocha@symfoni.com>
2022-05-24 20:54:26 +0200

    Janoszkowe zadanka

---
+++ zad5_3/src/pwj/mjanocha/z10/Main.java
@@ -0,0 +1,8 @@
+package pwj.mjanocha.z10;
+
+public class Main {
+  public static void main(String[] args) {
+    Graf g = new Graf("4, (0,1), (1,2), (3,0), (1,3)");
+    System.out.println(g);
+  }
+}
